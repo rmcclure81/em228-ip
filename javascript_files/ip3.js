038a8242bab41841d72a0e0df5ff32b949e35eee
Robert McClure <robertmcclure@Roberts-MacBook-Pro.local>
2020-08-28 11:09:15 -0600

    changed index.html

--- javascript_files/ip3.js
+++ javascript_files/ip3.js
@@ -1,50 +1,50 @@
-function printTitle() {
-  // if (window.XMLHttpRequest) {
-  //   // Code for IE7+, Firefox, Chrome, Opera, Safari
-  //   xmlhttp = new XMLHttpRequest();
-  // } else {
-  //   // Code for IE5, IE6
-  //   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-  // }
+// function printTitle() {
+// if (window.XMLHttpRequest) {
+//   // Code for IE7+, Firefox, Chrome, Opera, Safari
+//   xmlhttp = new XMLHttpRequest();
+// } else {
+//   // Code for IE5, IE6
+//   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+// }
 
-  var art = '../xml_data_files/art2.xml',
-    arthtml = '/em228-ip/html/art2.html',
-    artxsl = '/em228-ip/xsl_files/art2.xslt';
+// var art = '../xml_data_files/art2.xml',
+//   arthtml = '/em228-ip/html/art2.html',
+//   artxsl = '/em228-ip/xsl_files/art2.xslt';
 
-  if (
-    window.location.pathname == arthtml ||
-    window.location.pathname == artxsl
-  ) {
-    // if (window.XMLHttpRequest) {
-    //   // Code for IE7+, Firefox, Chrome, Opera, Safari
-    //   xmlhttp = new XMLHttpRequest();
-    // } else {
-    //   // Code for IE5, IE6
-    //   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-    // }
-    // xmlhttp.open('GET', 'art', false);
-    // xmlhttp.send();
-    // xmlDoc = xmlhttp.responseXML;
-    // document.write('<span>');
-    document.getElementById('headingTitle').innerHTML = 'Art Collection';
-    // document.write(
-    //   'Art Collection'
-    //   xmlDoc.getElementsByTagName('description')[0].childNodes[0].nodeValue
-    // );
-    // document.write('</span>');
-  } else {
-    document.getElementById('headingTitle').innerHTML = 'Wrong Path';
-  }
-  // document.getElementById('title').innerHTML = 'Art Collection';
+// if (
+//   window.location.pathname == arthtml ||
+//   window.location.pathname == artxsl
+// ) {
+// if (window.XMLHttpRequest) {
+//   // Code for IE7+, Firefox, Chrome, Opera, Safari
+//   xmlhttp = new XMLHttpRequest();
+// } else {
+//   // Code for IE5, IE6
+//   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+// }
+// xmlhttp.open('GET', 'art', false);
+// xmlhttp.send();
+// xmlDoc = xmlhttp.responseXML;
+// document.write('<span>');
+document.getElementById('headingTitle').innerHTML = 'Art Collection';
+// document.write(
+//   'Art Collection'
+//   xmlDoc.getElementsByTagName('description')[0].childNodes[0].nodeValue
+// );
+// document.write('</span>');
+// } else {
+//   document.getElementById('headingTitle').innerHTML = 'Wrong Path';
+// }
+// document.getElementById('title').innerHTML = 'Art Collection';
 
-  // xmlhttp.send();
-  // xmlDoc = xmlhttp.responseXML;
-  // document.write('<span>');
-  // document.write(
-  //   xmlDoc.getElementsByTagName('description')[0].childNodes[0].nodeValue
-  // );
-  // document.write('</span>');
-}
+// xmlhttp.send();
+// xmlDoc = xmlhttp.responseXML;
+// document.write('<span>');
+// document.write(
+//   xmlDoc.getElementsByTagName('description')[0].childNodes[0].nodeValue
+// );
+// document.write('</span>');
+// }
 
 // var z = xmlDoc.getElementsByTagName('image');
 // var d = xmlDoc.getElementsByTagName('date');
