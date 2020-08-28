5a27d768bc8e5d221cc30884686136f22fe2957a
Robert McClure <robertmcclure@Roberts-MacBook-Pro.local>
2020-08-28 12:41:13 -0600

    changed index.html

--- javascript_files/ip3.js
+++ javascript_files/ip3.js
@@ -7,7 +7,33 @@ function runScript() {
 // };
 
 function printTitle() {
-  document.getElementById('headingTitle').innerHTML = 'Art Collection';
+  if (window.XMLHttpRequest) {
+    // Code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    // Code for IE5, IE6
+    xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+  }
+
+  var art = '../xml_data_files/art2.xml',
+    arthtml = '/em228-ip/html/art2.html',
+    artxsl = '/em228-ip/xsl_files/art2.xslt';
+
+  if (
+    window.location.pathname == arthtml ||
+    window.location.pathname == artxsl
+  ) {
+    xmlhttp.open('GET', art, false);
+  } else {
+    document.getElementById('headingTitle').innerHTML = 'Wrong Path';
+  }
+  xmlhttp.send();
+  xmlDoc = xmlhttp.responseXML;
+  document.getElementById(
+    'headingTitle'
+  ).innerHTML = xmlDoc.getElementsByTagName(
+    'description'
+  )[0].childNodes[0].nodeValue;
 }
 
 // if (window.XMLHttpRequest) {
