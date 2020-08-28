622758abc0e7e2ebc00d83329135505624c96aa5
Robert McClure <robertmcclure@Roberts-MacBook-Pro.local>
2020-08-28 17:44:32 -0600

    changed index.html

--- javascript_files/ip3.js
+++ javascript_files/ip3.js
@@ -3,7 +3,6 @@ function runScript() {
   getAddress();
   timeModified();
   getDescription();
-  //   setDescription();
 }
 
 function printTitle() {
@@ -54,36 +53,36 @@ function getDescription() {
   ) {
     //** Creating GET Request for Art XML File **/
     xmlhttp.open('GET', art, false);
-    //   } else if (
-    //     //** Determining Path - Family **/
-    //     window.location.pathname == familyHTML ||
-    //     window.location.pathname == familyXSL
-    //   ) {
-    //     //** Creating GET Request for Family XML File **/
-    //     xmlhttp.open('GET', family, false);
-    //   } else if (
-    //     //** Determining Path - Movies **/
-    //     window.location.pathname == moviesHTML ||
-    //     window.location.pathname == moviesXSL
-    //   ) {
-    //     //** Creating GET Request for Movies XML File **/
-    //     xmlhttp.open('GET', movies, false);
-    //   } else if (
-    //     //** Determining Path - Music **/
-    //     window.location.pathname == musicHTML ||
-    //     window.location.pathname == musicXSL
-    //   ) {
-    //     //** Creating GET Request for Music XML File **/
-    //     xmlhttp.open('GET', music, false);
-    //   } else if (
-    //     //** Determining Path - Verses **/
-    //     window.location.pathname == versesHTML ||
-    //     window.location.pathname == versesXSL
-    //   ) {
-    //     //** Creating GET Request for Verses XML File **/
-    //     xmlhttp.open('GET', verses, false);
-    //   }
+  } else if (
+    //** Determining Path - Family **/
+    window.location.pathname == familyHTML ||
+    window.location.pathname == familyXSL
+  ) {
+    //** Creating GET Request for Family XML File **/
+    xmlhttp.open('GET', family, false);
+  } else if (
+    //** Determining Path - Movies **/
+    window.location.pathname == moviesHTML ||
+    window.location.pathname == moviesXSL
+  ) {
+    //** Creating GET Request for Movies XML File **/
+    xmlhttp.open('GET', movies, false);
+  } else if (
+    //** Determining Path - Music **/
+    window.location.pathname == musicHTML ||
+    window.location.pathname == musicXSL
+  ) {
+    //** Creating GET Request for Music XML File **/
+    xmlhttp.open('GET', music, false);
+  } else if (
+    //** Determining Path - Verses **/
+    window.location.pathname == versesHTML ||
+    window.location.pathname == versesXSL
+  ) {
+    //** Creating GET Request for Verses XML File **/
+    xmlhttp.open('GET', verses, false);
   }
+
   xmlhttp.send();
   xmlDoc = xmlhttp.responseXML;
   var desc = xmlDoc.getElementsByTagName('description')[0].childNodes[0]
@@ -98,14 +97,3 @@ function getDescription() {
 
   fourthDiv.appendChild(newSpan);
 }
-
-// function setDescription() {
-//   var newSpan = document.createElement('span');
-//   var allDiv = document.getElementsByTagName('div');
-//   var fourthDiv = allDiv[3];
-
-//   newSpan.setAttribute('id', 'description');
-//   newSpan.innerHTML = getDescription.desc;
-
-//   fourthDiv.appendChild(newSpan);
-// }
