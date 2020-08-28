ff8e729aede9b6691c1a5783a238f8803c725f0f
Robert McClure <robertmcclure@Roberts-MacBook-Pro.local>
2020-08-28 12:44:56 -0600

    changed index.html

--- javascript_files/ip3.js
+++ javascript_files/ip3.js
@@ -2,10 +2,6 @@ function runScript() {
   printTitle();
 }
 
-// document.getElementById('headingTitle').onload = function () {
-//   printTitle();
-// };
-
 function printTitle() {
   if (window.XMLHttpRequest) {
     // Code for IE7+, Firefox, Chrome, Opera, Safari
@@ -25,7 +21,8 @@ function printTitle() {
   ) {
     xmlhttp.open('GET', art, false);
   } else {
-    document.getElementById('headingTitle').innerHTML = 'Wrong Path';
+    document.getElementById('headingTitle').innerHTML =
+      window.location.pathname;
   }
   xmlhttp.send();
   xmlDoc = xmlhttp.responseXML;
@@ -35,66 +32,3 @@ function printTitle() {
     'description'
   )[0].childNodes[0].nodeValue;
 }
-
-// if (window.XMLHttpRequest) {
-//   // Code for IE7+, Firefox, Chrome, Opera, Safari
-//   xmlhttp = new XMLHttpRequest();
-// } else {
-//   // Code for IE5, IE6
-//   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-// }
-
-// var art = '../xml_data_files/art2.xml',
-//   arthtml = '/em228-ip/html/art2.html',
-//   artxsl = '/em228-ip/xsl_files/art2.xslt';
-
-// if (
-//   window.location.pathname == arthtml ||
-//   window.location.pathname == artxsl
-// ) {
-// if (window.XMLHttpRequest) {
-//   // Code for IE7+, Firefox, Chrome, Opera, Safari
-//   xmlhttp = new XMLHttpRequest();
-// } else {
-//   // Code for IE5, IE6
-//   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-// }
-// xmlhttp.open('GET', 'art', false);
-// xmlhttp.send();
-// xmlDoc = xmlhttp.responseXML;
-// document.write('<span>');
-// document.write(
-//   'Art Collection'
-//   xmlDoc.getElementsByTagName('description')[0].childNodes[0].nodeValue
-// );
-// document.write('</span>');
-// } else {
-//   document.getElementById('headingTitle').innerHTML = 'Wrong Path';
-
-// document.getElementById('title').innerHTML = 'Art Collection';
-
-// xmlhttp.send();
-// xmlDoc = xmlhttp.responseXML;
-// document.write('<span>');
-// document.write(
-//   xmlDoc.getElementsByTagName('description')[0].childNodes[0].nodeValue
-// );
-// document.write('</span>');
-// }
-
-// var z = xmlDoc.getElementsByTagName('image');
-// var d = xmlDoc.getElementsByTagName('date');
-// var i = 0;
-// document.write(art);
-// document.write(<br />);
-
-// document.write(window.location.pathname);
-
-// xmlhttp.open('GET', '../xml_data_files/art.xml', false);
-// xmlhttp.send();
-// xmlDoc = xmlhttp.responseXML;
-// document.write('<table border="5" bgcolor="lightgrey">');
-// var x = xmlDoc.getElementsByTagName('art');
-// var z = xmlDoc.getElementsByTagName('image');
-// var d = xmlDoc.getElementsByTagName('date');
-// var i = 0;
