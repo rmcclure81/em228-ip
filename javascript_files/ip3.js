959a5b79d5324218d68b6029e61391be0c7c769d
Robert McClure <robertmcclure@Roberts-MacBook-Pro.local>
2020-08-28 11:00:39 -0600

    changed index.html

--- javascript_files/ip3.js
+++ javascript_files/ip3.js
@@ -1,17 +1,4 @@
-// function printTitle() {
-// if (window.XMLHttpRequest) {
-//   // Code for IE7+, Firefox, Chrome, Opera, Safari
-//   xmlhttp = new XMLHttpRequest();
-// } else {
-//   // Code for IE5, IE6
-//   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-// }
-
-var art = '../xml_data_files/art2.xml',
-  arthtml = '/em228-ip/html/art2.html',
-  artxsl = '/em228-ip/xsl_files/art2.xslt';
-
-if (window.location.pathname == arthtml || window.location.pathname == artxsl) {
+function printTitle() {
   // if (window.XMLHttpRequest) {
   //   // Code for IE7+, Firefox, Chrome, Opera, Safari
   //   xmlhttp = new XMLHttpRequest();
@@ -19,19 +6,35 @@ if (window.location.pathname == arthtml || window.location.pathname == artxsl) {
   //   // Code for IE5, IE6
   //   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
   // }
-  // xmlhttp.open('GET', 'art', false);
-  // xmlhttp.send();
-  // xmlDoc = xmlhttp.responseXML;
-  // document.write('<span>');
-  document.getElementById('headingTitle').innerHTML = 'Art Collection';
-  // document.write(
-  //   'Art Collection'
-  //   xmlDoc.getElementsByTagName('description')[0].childNodes[0].nodeValue
-  // );
-  // document.write('</span>');
-} else {
-  document.getElementById('headingTitle').innerHTML = 'Wrong Path';
-  // }
+
+  var art = '../xml_data_files/art2.xml',
+    arthtml = '/em228-ip/html/art2.html',
+    artxsl = '/em228-ip/xsl_files/art2.xslt';
+
+  if (
+    window.location.pathname == arthtml ||
+    window.location.pathname == artxsl
+  ) {
+    // if (window.XMLHttpRequest) {
+    //   // Code for IE7+, Firefox, Chrome, Opera, Safari
+    //   xmlhttp = new XMLHttpRequest();
+    // } else {
+    //   // Code for IE5, IE6
+    //   xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+    // }
+    // xmlhttp.open('GET', 'art', false);
+    // xmlhttp.send();
+    // xmlDoc = xmlhttp.responseXML;
+    // document.write('<span>');
+    document.getElementById('headingTitle').innerHTML = 'Art Collection';
+    // document.write(
+    //   'Art Collection'
+    //   xmlDoc.getElementsByTagName('description')[0].childNodes[0].nodeValue
+    // );
+    // document.write('</span>');
+  } else {
+    document.getElementById('headingTitle').innerHTML = 'Wrong Path';
+  }
   // document.getElementById('title').innerHTML = 'Art Collection';
 
   // xmlhttp.send();
