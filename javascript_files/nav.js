4ec2e018ce942a53a7c5ee0fafee888cde5f6526
Robert McClure <robertmcclure@Roberts-MacBook-Pro.local>
2020-08-27 22:06:56 -0600

    changed index.html

--- javascript_files/nav.js
+++ javascript_files/nav.js
@@ -1,4 +1,4 @@
-//** Week 1 **//
+//** Week 1 -  **//
 document.write('<div class="dropdown" id="w1">');
 document.write(
   '<button class="btn_dropdown_toggle" type="button" data-toggle="dropdown">'
